0fc28f146cf19a537b841739729e46fdcdeef8f5
Tom Yeoman <tom.yeoman@live.co.uk>
2021-05-12 12:46:27 +0100

    Basic redux toolkit added

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 import Main from './game/Main';
+import { Counter } from './features/counter/Counter';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Main />
+        <Provider store={store}>
+
+      <Main />
+      <Counter/>
+      </Provider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
